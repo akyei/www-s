14e04157f39d5d5e2abc81074d0ebeda1ac8a107
Alexander Kyei <alex.kyei94@gmail.com>
2015-03-13 04:12:05 -0400

    Finished website, pushing files in a big batch.

--- email.php
+++ email.php
@@ -4,7 +4,7 @@ require('class.phpmailer.php');
 PHPMailerAutoload("smtp");
 
 $mail = new PHPMailer;
-$phil_chair = 'alex.kyei94@gmail.com';
+$phil_chair = 'jkagan95@gmail.com';
 if (isset($_POST['body'], $_POST['subject'], $_POST['info-name'], $_POST['info-org'], $_POST['info-email'])){
 	$subject = $_POST['subject'];
 	$message = $_POST['body'];
@@ -12,23 +12,23 @@ if (isset($_POST['body'], $_POST['subject'], $_POST['info-name'], $_POST['info-o
 	$org = $_POST['info-org'];
 	$email = $_POST['info-email'];
 	$body = <<<BODY
-$message\n
-\n
-\n
-Contact Info \n
-Name: $name \n
-Organization: $org \n
+$message\n<br>
+\n<br>
+\n<br>
+Contact Info \n<br>
+Name: $name \n<br>
+Organization: $org \n<br>
 Email: $email 
 BODY;
 }
 $mail->SMTPDebug = 1;
 $mail->isSMTP();
-$mail->HOST = 'smtp.gmail.com';
-$mail->SMTPAuth = true;
-$mail->Username = 'betaiotaspd@gmail.com';
-$mail->Password = 'whatsoeveristrue';
-$mail->SMTPSecure = 'ssl';
-$mail->Port = 465;
+$mail->HOST = 'exch.mail.umd.edu';
+//$mail->SMTPAuth = true;
+$mail->Username = 'akyei';
+$mail->Password = 'Atrainrulesall1a!!';
+//$mail->SMTPSecure = 'ssl';
+$mail->Port = 25;
 
 $mail->From = $email;
 $mail->FromName = $name;
@@ -44,6 +44,6 @@ if (!$mail->send()){
 	echo 'Message could not be sent.';
 	echo 'Mailer Error: ' . $mail->ErrorInfo;
 } else {
-	header('Location: index.html');
+	header('Location: default.htm');
 }   
 ?>
